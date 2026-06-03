74c59fc90fa7a67d0b75a76c838741e0bcfde51b
Demy Besten <demy.besten@friday.nl>
2026-06-03 19:35:56 +0200

    fix analytics import to use next-specific export

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,8 @@ import Footer from '@/components/Footer'
 import { Toaster } from 'react-hot-toast'
 import { getTranslations, detectLocale } from '@/lib/i18n-server'
 import { TranslationProvider } from '@/lib/i18n-client'
-import { Analytics } from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,26 +22,27 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default async function RootLayout({
-  children,
-}: {
+                                           children,
+                                         }: {
   children: React.ReactNode
 }) {
   const locale = await detectLocale()
   const t = await getTranslations(locale)
 
   return (
-    <html lang={locale}>
+      <html lang={locale}>
       <body className={inter.className}>
-        <TranslationProvider dictionary={t} initialLocale={locale}>
-          <Toaster position="top-center" />
-          <Navigation />
-          <main className="min-h-screen">
-            {children}
-          </main>
-          <Footer />
-        </TranslationProvider>
-          <Analytics />
+      <TranslationProvider dictionary={t} initialLocale={locale}>
+        <Toaster position="top-center" />
+        <Navigation />
+        <main className="min-h-screen">
+          {children}
+        </main>
+        <Footer />
+      </TranslationProvider>
+      <Analytics />
+      <SpeedInsights/>
       </body>
-    </html>
+      </html>
   )
 }
